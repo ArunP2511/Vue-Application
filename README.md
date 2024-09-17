218283275fe0158c7058c5e3434a0d81792768a0
ArunP2511 <arunp418@gmail.com>
2024-09-17 14:55:19 +0530

    comments added

---
+++ README.md
@@ -0,0 +1,66 @@
+
+# Vue fullstack
+
+## Description
+
+This is a full-stack web application built with Vue.js on the frontend and Node.js (Express.js) on the backend.It shows the graphical representation of the tree hierarchical structure.
+
+## Technologies Used
+
+## Frontend
+`Vue.js` - JavaScript framework for building user interfaces
+Axios (for HTTP requests)
+D3.js
+
+## Backend
+
+Node.js - JavaScript runtime environment
+Express.js - Web framework for Node.js
+neo4j (or any other database) for data storage
+
+
+## Project setup
+
+### Clone the repository
+
+```shell
+# Clone the repository
+$ git clone https://github.com/your-username/your-repo-name.git
+$ cd your-repo-name
+```
+### Install dependencies
+
+###  For the frontend (Vue.js):
+
+```shell
+
+$ cd vue-frontend
+$ npm install
+```
+
+###  For the backend (node.js):
+```shell
+$ cd vue-backend
+$ npm install
+```
+
+### Running the Application
+
+### 1. Run the backend server:
+
+Navigate to the backend directory and run:
+
+```shell
+npm run dev
+```
+
+This will start the Node.js backend on http://localhost:5713.
+
+### 2 .Run the frontend (Vue) application:
+
+Navigate to the fronend directory and run:
+
+```shell
+npm run server
+```
+This will start the Vue development server on http://localhost:3100.
--- vue-backend/server.js
+++ vue-backend/server.js
@@ -1,6 +1,7 @@
+// import expressjs
 const express = require("express");
 const myApp = express();
-
+// import cors drivers for cross origin
 const cors = require("cors");
 const corsValue = {
   origin: "*",
@@ -8,31 +9,39 @@ const corsValue = {
   optionSuccessStatus: true,
 };
 myApp.use(cors(corsValue));
-
+//import the neo4j driver
 const myneo4j = require("neo4j-driver");
 
 myApp.get("/data", async (req, res) => {
   try {
+    // neo4j Aura database connection details
     NEO4J_URI = "neo4j+s://9ed55a1a.databases.neo4j.io";
     NEO4J_USERNAME = "neo4j";
     NEO4J_PASSWORD = "Cq-8P2z7oImWyUbVMJIS8_-q23OAGnFeLZeB1JpiIO4";
+    // Create a driver instance, for the specified Neo4j instance
     const myDriver = myneo4j.driver(
       NEO4J_URI,
       myneo4j.auth.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
     );
+    //create a session
     const mySession = myDriver.session();
+    //give a cypher query to get the data from neo4j
     const myQuery = `MATCH(n:Entity)
                        OPTIONAL MATCH (n)<-[:PARENT]-(p:Entity)
                        RETURN n.name AS name, n.description AS description, COALESCE(p.name, '') AS parent
                        ORDER BY n.name`;
+    //run the query through the session 
     const myResult = await mySession.run(myQuery);
     const myData = myResult.records.map((record) => ({
       name: record.get("name"),
       description: record.get("description"),
       parent: record.get("parent"),
     }));
+    // return the result
     res.json(myData);
+    //close the session
     await mySession.close();
+    //close the driver
     await myDriver.close();
   } catch (error) {
     res.status(500).json({ error: "Error while fetching data from neo4j" });
